e128abdcf7000016ec13c832315692e9c15653b6
Francisco Renteria <renteriav@hotmail.com>
2015-10-13 20:58:41 -0700

    fine tune Amazon template

--- app/controllers/tesseract_controller.rb
+++ app/controllers/tesseract_controller.rb
@@ -2,7 +2,7 @@ class TesseractController < ApplicationController
   skip_before_filter :verify_authenticity_token
   before_action :authenticate_user!
   before_action :set_qb_service, only: [:create_purchase, :upload]
-  require 'tesseract'
+  #require 'tesseract'
   require 'RMagick'
   def index
   end
@@ -19,10 +19,11 @@ class TesseractController < ApplicationController
     brightness = params[:brightness].to_i
     #%x(mkdir public/tessdir)
     
-    e = Tesseract::Engine.new {|e|
-      e.language  = :eng
-      e.blacklist = '|'
-    }
+    
+    #e = Tesseract::Engine.new {|e|
+    #  e.language  = :eng
+    #  e.blacklist = '|'
+    #}
     
     (brightness < 220) ? brightness_increase = ((220 - brightness).to_f / 130) : brightness_increase = 0
     increased_brightness = 1 + brightness_increase
@@ -44,25 +45,33 @@ class TesseractController < ApplicationController
       while counter < image.size do 
         page = RTesseract.read("public/uploads/#{file_name}-#{counter}.jpg") do |img|
           img = img.quantize(256,Magick::GRAYColorspace)
-          #img = img.modulate(increased_brightness)
+          img = img.modulate(1.3)
           img.write("public/uploads/#{file_name}-#{counter}.jpg")
-          lines = e.lines_for("public/uploads/#{file_name}-#{counter}.jpg")
+          #lines = e.lines_for("public/uploads/#{file_name}-#{counter}.jpg")
+          text = RTesseract.new("public/uploads/#{file_name}-#{counter}.jpg", {psm: 6})
+          text = text.to_s
+          lines = text.split("\n")
           lines.each do |line|
-            @lines.push(line.to_s)
+            @lines.push(line) unless line == "" 
           end
           counter += 1
         end
       end
+      puts "#{@lines}"
     else
       page = RTesseract.read("public/uploads/#{file_name}.jpg") do |img|
         img = img.quantize(256,Magick::GRAYColorspace)
-        #img = img.modulate(increased_brightness)
+        img = img.modulate(1.3)
         img.write("public/uploads/#{file_name}.jpg")
-        lines = e.lines_for("public/uploads/#{file_name}.jpg")
+        #lines = e.lines_for("public/uploads/#{file_name}.jpg")
+        text = RTesseract.new("public/uploads/#{file_name}.jpg", {psm: 6})
+        text = text.to_s
+        lines = text.split("\n")
         lines.each do |line|
-          @lines.push(line.to_s)
+          @lines.push(line) unless line == "" 
         end
       end
+      
     end
 
     amazon_template(@lines)
@@ -79,7 +88,7 @@ class TesseractController < ApplicationController
     #@result2 = contents
   end
   
-  def create_expense(date, vendor_id, bank_account_id, doc_number, total, items={})
+  def create_expense(date, vendor_id, bank_account_id, product_name, doc_number, total, items={})
     @qbo_client = current_user.qbo_client
     
     entity_ref_id = vendor_id
@@ -90,27 +99,36 @@ class TesseractController < ApplicationController
     service.company_id = @qbo_client.realm_id
     
     products = get_items
+    existing_products = products.select {|product| product[:product].downcase == product_name.downcase}
+    if existing_products.any?
+      item_ref = Quickbooks::Model::BaseReference.new(existing_products.first[:id])
+    else
+      create_item(product_name)
+      new_product = {product: @created_item.name, id: @created_item.id}
+      item_ref = Quickbooks::Model::BaseReference.new(@created_item.id)
+    end
+    
     items.each do |item|
       line_item = Quickbooks::Model::PurchaseLineItem.new
       line_item.description = item[:description]
       line_item.amount = item[:amount]
-
       line_item.item_based_expense! do |line|
-        existing_products = products.select {|product| product[:product].downcase == item[:product].downcase}
-        if existing_products.any?
-          item_ref = Quickbooks::Model::BaseReference.new(existing_products.first[:id])
-        else
-          create_item(item[:product])
-          new_product = {product: @created_item.name, id: @created_item.id}
-          products.push(new_product)
-          item_ref = Quickbooks::Model::BaseReference.new(@created_item.id)
-        end
         line.item_ref = item_ref
         line.unit_price = item[:price]
         line.quantity = item[:quantity]
       end
       purchase.line_items << line_item
+      
     end
+    
+    #txn_tax_detail = Quickbooks::Model::TransactionTaxDetail.new
+    #tax_line = Quickbooks::Model::TaxLine.new
+    #tax_line.detail_type = "TaxLineDetail"
+    #tax_line.amount = 2.35
+    #txn_tax_detail.total_tax = 2.34
+    #txn_tax_detail.lines = [tax_line]
+    #purchase.txn_tax_detail = txn_tax_detail
+    
 
     entity_ref = Quickbooks::Model::BaseReference.new(entity_ref_id)
     purchase.entity_ref = entity_ref
@@ -135,22 +153,77 @@ class TesseractController < ApplicationController
   end
   
   def amazon_template(lines)
+    #clean array
+    lines.delete_if { |e| /http/ =~ e }
+    lines.delete_if { |e| /PM/ =~ e }
+    lines.delete_if { |e| /AM/ =~ e }
     #order #
-    order_line = lines.find { |e| /Details for Order/ =~ e }
+    order_line = lines.find { |e| /order number/ =~ e }
     if order_line
-      @order_number = order_line.split('#').last
+      @order_number = order_line.split(':').last
+      @order_number.gsub!("—", "-")
+      @order_number.gsub!("O", "0")
     end
     # Grand Total
     total_line = lines.find { |e| /Order Total/ =~ e }
     if total_line
       @total = total_line.split('$').last
     end
+    #shipping and handling
+    shipping_lines = lines.each_index.select{|i| (lines[i] =~ /andling/) || (lines[i] =~ /andiing/) || (lines[i] =~ /and\|ing/)}
+    if shipping_lines.any?
+      @total_shipping = lines[shipping_lines.last].split("$").last
+      @total_shipping.gsub!("g", "9")
+      @total_shipping.gsub!("_", ".")
+      if /\./.match(@total_shipping).nil?
+        @total_shipping.insert(-3, '.')
+      end
+    end
+    #Super saver discount
+    saver_lines = lines.each_index.select{|i| lines[i] =~ /Super Saver/i}
+    if saver_lines.any?
+      @total_saver = lines[saver_lines.last].split("$").last
+      if /\./.match(@total_saver).nil?
+        @total_saver.insert(-3, '.')
+      end
+    end
+    #giftcard
+    giftcard_lines = lines.each_index.select{|i| (lines[i] =~ /Gift Card Amount/i)}
+    if giftcard_lines.any?
+      @total_giftcard = lines[giftcard_lines.last].split("$").last
+      @total_giftcard.gsub!("g", "9")
+      @total_giftcard.gsub!("_", ".")
+      if /\./.match(@total_giftcard).nil?
+        @total_giftcard.insert(-3, '.')
+      end
+    end
+    #tax
+    tax_lines = lines.each_index.select{|i| lines[i] =~ /Sales Tax/i}
+    if tax_lines.any?
+      @total_tax = (0).to_f
+      tax_lines.each do |line|
+        tax = lines[line].split('$').last
+        if /\./.match(tax).nil?
+          tax.insert(-3, '.')
+        end
+        @total_tax += tax.to_f
+      end
+    else
+      tax_line = lines.find { |e| /Tax Collected/i =~ e }
+      if tax_line
+        @total_tax = tax_line.split('$').last.to_f
+      end
+    end
+    #Rewards Points
+    rewards_line = lines.find { |e| /Rewards Points/i =~ e }
+    if rewards_line
+      @total_rewards = rewards_line.split('$').last
+    end
     #items
     @items = Array.new
     items_header_lines = lines.each_index.select{|i| lines[i] =~ /Items Ordered/}
-    items_subtotal_lines = lines.each_index.select{|i| lines[i] =~ /Item\(s\) Subtotal/}
-    
-    puts "#{items_header_lines}"
+    items_subtotal_lines = lines.each_index.select{|i| lines[i] =~ /tem\(s\) Subtota/}
+    tax_lines = lines.each_index.select{|i| lines[i] =~ /Sales Tax/i}
     
     if items_header_lines && items_subtotal_lines
       items_header_lines.each do |item|
@@ -160,34 +233,72 @@ class TesseractController < ApplicationController
         i = items_initial_index
         while i < items_ending_index
           item_hash = Hash.new
-          item_hash[:product] = "Amazon Purchase"
-          item_hash[:amount] = lines[i].split("$").last
+          item_hash[:price] = lines[i].split("$").last
           description = lines[i].split("$").first
-          if /of:/.match(description)
+          if /of:/i.match(description)
+            description.gsub!(/of:/i, "of:")
             item_hash[:description] = description.split("of:").last
             item_hash[:quantity] = description.split("of:").first
           else
             item_hash[:description] = description
             item_hash[:quantity] = 1
           end
+          item_hash[:amount] = sprintf "%.2f", item_hash[:quantity].to_f * item_hash[:price].to_f
           
           i += 1
           while /Sold by/i.match(lines[i]).nil?
-            item_hash[:description] += " "+ lines[i]
+            item_hash[:description] += " " + lines[i]
             i += 1
           end
           
-          item_hash[:seller] = lines[i].split("(").first
+          if /\(/.match(lines[i])
+            item_hash[:seller] = lines[i].split("(").first
+          else
+            item_hash[:seller] = lines[i].split("seller").first
+          end
           i += 1
-          item_hash[:condition] = lines[i]
-          i += 1
+          if /\$/.match(lines[i]).nil?
+            item_hash[:condition] = lines[i]
+            i += 1
+          end
           while /\$/.match(lines[i]).nil?
-            item_hash[:condition] += " "+ lines[i]
+            item_hash[:condition] += " "+ lines[i] unless /Eligible/i.match(lines[i]) || /Ship/i.match(lines[i]) || /Send/i.match(lines[i])
             i += 1
           end
           @items.push(item_hash)
         end
       end
+    end
+    
+    if @total_shipping.to_f > 0
+      item_hash = Hash.new
+      item_hash[:description] = "Shipping and Handling"
+      item_hash[:amount] = @total_shipping
+      @items.push(item_hash)
+    end
+    if @total_saver
+      item_hash = Hash.new
+      item_hash[:description] = "Super Saver Discount"
+      item_hash[:amount] = @total_saver.to_f * (-1)
+      @items.push(item_hash)
+    end
+    if @total_rewards && @total_rewards.to_f > 0
+      item_hash = Hash.new
+      item_hash[:description] = "Rewards Points"
+      item_hash[:amount] = @total_rewards.to_f * (-1)
+      @items.push(item_hash)
+    end
+    if @total_giftcard && @total_giftcard.to_f > 0
+      item_hash = Hash.new
+      item_hash[:description] = "Gift Card"
+      item_hash[:amount] = @total_giftcard.to_f * (-1)
+      @items.push(item_hash)
+    end
+    if @total_tax && @total_tax.to_f > 0
+      item_hash = Hash.new
+      item_hash[:description] = "Sales Tax"
+      item_hash[:amount] = sprintf "%.2f", @total_tax
+      @items.push(item_hash)
     end
     
     #date
@@ -198,9 +309,8 @@ class TesseractController < ApplicationController
     elsif digital_date_line
       @date = Date.parse(digital_date_line.split(':').last.strip)
     end
-    
-    create_expense(@date, 78, 42, @order_number, @total, @items)
-    
+    create_expense(@date, 78, 42, "Amazon Purchase", @order_number, @total, @items)
+    #render_response(true, "Upload succesfull", 200)
   end
   
   def create_item(name)
